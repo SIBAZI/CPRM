3d9e9ce4df73620ed28fbf83f9469fb0991338d4
sibazi <yuzu.taichi.shiba@gmail.com>
2024-01-11 23:48:58 +0900

    1/11

---
+++ pages/login.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      showPassword: false,
+      name: "",
+      password: "",
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.name, this.password);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <v-app>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">ログイン</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              prepend-icon="mdi-account-circle"
+              label="ユーザ名"
+              v-model="name"
+            />
+            <v-text-field
+              v-bind:type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              label="パスワード"
+              @click:append="showPassword = !showPassword"
+              v-model="password"
+            />
+            <v-card-actions>
+              <v-btn color="info" @click="submit">ログイン</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-app>
+  </div>
+</template>
