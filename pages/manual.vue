9c1c90ae78f2f21bd188a1f8f9e9385b4ab8ca8a
sibazi <yuzu.taichi.shiba@gmail.com>
2024-10-25 00:41:32 +0900

    現在作成中追加

--- pages/manual.vue
+++ pages/manual.vue
@@ -709,6 +709,12 @@ export default {
             ></v-select>
           </v-col>
         </v-row>
+        <v-row v-if="this.start.startKubun === '買取・下取り'">
+          <p class="ml-16">　※現在作成中</p>
+        </v-row>
+        <v-row v-if="this.start.startKubun === '一般登録代行'">
+          <p class="ml-16">　※現在作成中</p>
+        </v-row>
       </div>
       <div v-if="this.start.startKubun === '成約時登録'">
         <v-row>
