78574fd72eaf7683f84ed82273d942e966ce2b45
sibazi <yuzu.taichi.shiba@gmail.com>
2025-01-19 00:23:03 +0900

    1/19

--- pages/manual.vue
+++ pages/manual.vue
@@ -1243,7 +1243,7 @@ export default {
             <p class="supporting-sentences">　※現在作成中</p>
           </v-row>
           <v-row v-if="this.start.startKubun === '一般登録代行'">
-            <p class="supporting-sentences">　※現在作成中（LastUpdated.2025.01.14.AM02:51）</p>
+            <p class="supporting-sentences">　※現在作成中（LastUpdated.2025.01.19.AM02:51）</p>
           </v-row>
         </div>
         <div v-if="this.start.startKubun === '成約時登録'">
